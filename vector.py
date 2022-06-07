6ba30198a1b64936efc1414ece14646e4ba212bc
Antoni <107063507+JENOT-ANT@users.noreply.github.com>
2022-06-07 23:21:21 +0200

    Update vector.py

--- vector.py
+++ vector.py
@@ -6,6 +6,7 @@ Author: JENOT
 
 import math
 
+
 class Vector(object):
     '''2D vector object'''
 
@@ -16,11 +17,16 @@ class Vector(object):
         self.x = coordinate_x
         self.y = coordinate_y
 
+    def _2_tuple(self):
+        return (self.x, self.y)
+
     def get_value(self):
         return math.sqrt(pow(self.x, 2) + pow(self.y, 2))  # _/x^2 + y^2`
 
+
 def add(v1: Vector, v2: Vector):
     return Vector(v1.x + v2.x, v1.y + v2.y)
+
 
 def subtract(v1: Vector, v2: Vector):
     return Vector(v1.x - v2.x, v1.y - v2.y)
