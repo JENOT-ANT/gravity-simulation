88af0a2822a38eadfcfe3a629b58fe06c2f94703
Antoni <107063507+JENOT-ANT@users.noreply.github.com>
2022-06-07 22:09:07 +0200

    Create vector.py

---
+++ vector.py
@@ -0,0 +1,26 @@
+'''
+Basic library defining 2D vector object and some basic operations on vectors.
+
+Author: JENOT
+'''
+
+import math
+
+class Vector(object):
+    '''2D vector object'''
+
+    x: float = None
+    y: float = None
+
+    def __init__(self, coordinate_x: float, coordinate_y: float):
+        self.x = coordinate_x
+        self.y = coordinate_y
+
+    def get_value(self):
+        return math.sqrt(pow(self.x, 2) + pow(self.y, 2))  # _/x^2 + y^2`
+
+def add(v1: Vector, v2: Vector):
+    return Vector(v1.x + v2.x, v1.y + v2.y)
+
+def subtract(v1: Vector, v2: Vector):
+    return Vector(v1.x - v2.x, v1.y - v2.y)
