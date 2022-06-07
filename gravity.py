3306f73ec1769e8923c2fe02e403d9b09eb5385c
Antoni <107063507+JENOT-ANT@users.noreply.github.com>
2022-06-07 23:12:35 +0200

    Update gravity.py

--- gravity.py
+++ gravity.py
@@ -1,4 +1,21 @@
-import vector
+from vector import *
+
 
 class Object(object):
-    pass
+    '''material object class'''
+
+    mass:           float = None
+    position:       Vector = None
+    force:          Vector = None
+    acceleration:   Vector = None
+    velocity:       Vector = None
+
+    radius:         float = None
+    color:          tuple = None
+
+    def __init__(self, object_mass: float, object_position: Vector, radius: float, color: tuple):
+        self.mass = object_mass
+        self.position = object_position
+        self.radius = radius
+        self.color = color
+    
