209eb63ac70c00e0ae4d241a983181a4c6ce01db
Antek <antekswitaj@outlook.com>
2022-06-12 09:10:47 +0200

    Update "gravity.py".

--- gravity.py
+++ gravity.py
@@ -1,30 +1,28 @@
 import math
 import vector as v
 
-G = 6.6743015 * pow(10, -17)  # using kilometer^3
+G = 6.6743015 * pow(10, -17)  # for [km]
 
 
 class Object(object):
     """material object class"""
 
-    mass: float = None
-    position: v.Vector = None
-    force: v.Vector = None
-    acceleration: v.Vector = None
-    velocity: v.Vector = None
+    mass: float = None # [kg]
+    position: v.Vector = None # [km]
+    force: v.Vector = None # [N]
+    acceleration: v.Vector = None # [km / t^2]
+    velocity: v.Vector = None # [km / t]
 
-    radius: float = None
-    color: tuple = None
+    radius: float = None # [km]
 
     def __init__(
-        self, object_mass: float, object_position: v.Vector, radius: float, color: tuple
+        self, object_mass: float, object_position: v.Vector, object_radius: float
     ):
         self.mass = object_mass
         self.position = object_position
-        self.radius = radius
-        self.color = color
+        self.radius = object_radius
 
-    def _calculate_gravity_one_object_(self, influancing_object):
+    def _one_object_gravity_(self, influancing_object):
         angle: float = None
         gravity: v.Vector = v.Vector(0, 0)
 
@@ -46,7 +44,32 @@ class Object(object):
         return gravity
 
     def calculate_gravity(self, influacing_objects: tuple):
-        gravity_value: float = None
+        gravity: v.Vector = v.Vector(0, 0)
 
         for influanceing_object in influacing_objects:
-            pass
+            gravity = v.add(gravity, self._one_object_gravity_(influanceing_object))
+
+        return gravity
+
+    def calculate_acceleration(self):
+        acceleration: v.Vector = v.Vector(
+            self.force.x / self.mass, self.force.y / self.mass
+        )
+
+        return acceleration
+
+    def update_velocity(self, velocity: v.Vector):
+        velocity = v.add(velocity, self.acceleration)
+
+        return velocity
+
+    def update_position(self, position: v.Vector):
+        position = v.add(position, self.velocity)
+
+        return position
+
+    def update(self, influencing_objects: tuple):
+        self.force = self.calculate_gravity(influencing_objects)
+        self.acceleration = self.calculate_acceleration()
+        self.velocity = self.update_velocity(self.velocity)
+        self.position = self.update_position(self.position)
