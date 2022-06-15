161d492b095c32709c8d37a9ab53c47eaa9ff9a3
Antek <antekswitaj@outlook.com>
2022-06-15 20:48:15 +0200

    Add "gravity-engine.py" file.

---
+++ gravity-engine.py
@@ -0,0 +1,80 @@
+import gravity
+import pygame
+import vector
+
+pygame.init()
+OFF = 0
+ON = 1
+PAUSE = 2
+
+
+class Color(object):
+    red: int = None
+    green: int = None
+    blue: int = None
+
+    def __init__(self, red, green, blue):
+        self.red = red
+        self.green = green
+        self.blue = blue
+
+    def __call__(self):
+        return (self.red, self.green, self.blue)
+
+
+class Window(object):
+    resolution = None
+    display: pygame.Surface = None
+    frame_color: tuple = None
+
+    def __init__(self, window_resolution: tuple):
+        self.display = pygame.display.set_mode(self.resolution)
+
+    def update(self):
+        self.display.blits()
+        self.display.fill((0, 0, 0))
+
+
+class Scene(object):
+    objects: list = None
+
+    def __init__(self):
+        pass
+
+    def add_object(self):
+        pass
+
+    def update(self):
+        pass
+
+
+class Cam(object):
+    translation: vector.Vector = None
+    scale: int = None
+
+    def __init__(self, translation, scale):
+        pass
+
+
+class Simulation(object):
+
+    window: Window = None
+    cam: Cam = None
+    scene: Scene = None
+    state: int = None
+
+    def __init__(self):
+        self.window = Window()
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quite()
+
+    def main_loop(self):
+        while self.state != 0:
+            pygame.clock.tick(self.frame_rate)
+            
+            self.handle_events()
+
+            self.window.update()
--- gravity.py
+++ gravity.py
@@ -67,7 +67,7 @@ class Object(object):
         for influanceing_object in influacing_objects:
             distance: v.Vector = v.subtract(influanceing_object.position, self.position)
             
-            if distance.get_value() =< (self.radius + influanceing_object.radius) :
+            if distance.get_value() <= (self.radius + influanceing_object.radius) :
                 velocity.x = (velocity.x * (self.mass - influanceing_object.mass) + (2 * influanceing_object.mass * influanceing_object.velocity.x)) / (self.mass + influanceing_object.mass)
                 velocity.y = (velocity.y * (self.mass - influanceing_object.mass) + (2 * influanceing_object.mass * influanceing_object.velocity.y)) / (self.mass + influanceing_object.mass)
 
